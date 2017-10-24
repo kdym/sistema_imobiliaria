a063c3c9e8b8143ea35e5109731137ce4dd25682
Kdym Soluçoes <kdymsolucoes@gmail.com>
2017-10-24 10:46:25 -0200

    Certificado SSL pro GoogleMaps

--- src/Controller/Component/GoogleMapsComponent.php
+++ src/Controller/Component/GoogleMapsComponent.php
@@ -27,7 +27,7 @@ class GoogleMapsComponent extends Component
 
         $context = stream_context_create(array('ssl' => array(
             'verify_peer' => true,
-            'cafile' => WWW_ROOT . '/ca-bundle.crt'
+            'cafile' => 'https://raw.githubusercontent.com/bagder/ca-bundle/master/ca-bundle.crt'
         )));
         libxml_set_streams_context($context);
 
