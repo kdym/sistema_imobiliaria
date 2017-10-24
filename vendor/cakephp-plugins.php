25c6a496eb76d5f830a83c6397a15e8ff130dbb9
Kdym Soluçoes <kdymsolucoes@gmail.com>
2017-10-24 09:44:15 -0200

    Adição da pasta vendor para publicação

---
+++ vendor/cakephp-plugins.php
@@ -0,0 +1,12 @@
+<?php
+$baseDir = dirname(dirname(__FILE__));
+return [
+    'plugins' => [
+        'AdminLTE' => $baseDir . '/vendor/maiconpinto/cakephp-adminlte-theme/',
+        'Bake' => $baseDir . '/vendor/cakephp/bake/',
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
+        'SoftDelete' => $baseDir . '/vendor/pgbi/cakephp3-soft-delete/',
+        'TwitterBootstrap' => $baseDir . '/vendor/cakephp-brasil/twitter-bootstrap/'
+    ]
+];
